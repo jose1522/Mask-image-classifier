b9d4c5db7e49f499c53f214e0ad89ac841d0625b
Jose Andres Moya Porras <jose.91cr@gmail.com>
2020-08-11 23:46:47 -0600

    Experimenting with parameters

--- Mask Recognition.R
+++ Mask Recognition.R
@@ -20,15 +20,23 @@ image_options <- c("with_mask","no_mask")
 output_n <- length(image_options)
 
 train_data_gen = image_data_generator(
-  rescale = 1/255
+  rescale = 1/255,
+  featurewise_center=TRUE,
+  featurewise_std_normalization=TRUE,
+  rotation_range=45
 )
 
 test_data_gen <- image_data_generator(
-  rescale = 1/255
+  rescale = 1/255,
+  featurewise_center=TRUE,
+  featurewise_std_normalization=TRUE,
+  rotation_range=45
 )
 
 mask_eval_gen = image_data_generator(
-  rescale = 1/255
+  rescale = 1/255,
+  featurewise_center=TRUE,
+  featurewise_std_normalization=TRUE
 )
 
 
@@ -74,39 +82,53 @@ table(factor(testImages$classes))
 train_samples <- trainingImages$n
 test_samples <- testImages$n
 batch_size <- 32
-epochs <- 25
+epochs <- 40
 
 model <- keras_model_sequential() %>%
   layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid', input_shape = c(img_width, img_height, channels)) %>%
   layer_activation('relu') %>%
   layer_batch_normalization() %>%
-  
+
   # Second hidden layer
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_activation('relu') %>%
+  layer_dropout(0.20) %>%
+  
+  # Third hidden layer
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_activation('relu') %>%
+
+  # Third hidden layer
   layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
   layer_activation('relu') %>%
   
   # Third hidden layer
-  layer_conv_2d(filter = 16, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_activation('relu') %>%
+  layer_dropout(0.20) %>%
+
+  # Third hidden layer
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
   layer_activation('relu') %>%
   
   # Fourth hidden layer
-  layer_conv_2d(filter = 16, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
   layer_activation('relu') %>%
   
   # Fifth hidden layer
-  layer_conv_2d(filter = 16, kernel_size = c(3,3), padding = 'valid') %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = 'valid') %>%
   layer_activation('relu') %>%
   
   # Use max pooling
   layer_max_pooling_2d(pool_size = c(3,3)) %>%
-  layer_dropout(0.25) %>%
+  layer_dropout(0.20) %>%
   
   # Flatten max filtered output into feature vector 
   # and feed into dense layer
   layer_flatten() %>%
-  layer_dense(30) %>%
+  layer_dense(50) %>%
   layer_activation('relu') %>%
-  layer_dropout(0.50) %>%
+  layer_dropout(0.20) %>%
 
   layer_activation('relu') %>%
   # Outputs from dense layer are projected onto output layer
@@ -136,7 +158,7 @@ hist <- model %>% fit_generator(
   validation_steps = as.integer(test_samples / batch_size),
   
   # print progress
-  verbose = 2,
+  verbose = 0,
   callbacks = list(
     # save best model after every epoch
     callback_model_checkpoint(file.path(path, "mask_checkpoints.h5"), save_best_only = TRUE),
@@ -158,7 +180,6 @@ predictions <- predict_generator(model, mask_eval, steps=length(mask_eval))
 predictions <- transform(predictions, predicted_class=apply(predictions, 1, which.max)-1)
 predictions['original_classes'] <- mask_eval$classes
 predictions <- predictions[,3:4]
-mask_eval$class_indices
 
 confusionMatrix(
   factor(predictions$predicted_class, labels=c('with','without')),
